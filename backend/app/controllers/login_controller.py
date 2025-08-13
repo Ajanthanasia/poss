343236a517c43263672bf5533b9da049353c73dc
Ajanthan <ajanthan.dev@hotelshippo.lk>
2025-08-13 12:45:10 +0530

    signup validation update

--- backend/app/controllers/login_controller.py
+++ backend/app/controllers/login_controller.py
@@ -5,30 +5,44 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app.services.jwt import generate_jwt
 
 def register_admin(data):
-    username = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
+    try:
+        username = data.get('username')
+        email = data.get('email')
+        password = data.get('password')
 
-    db = SessionLocal()
-    
-    hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
+        db = SessionLocal()
 
-    new_user = User(
-        name=username,
-        email=email,
-        password=hashed_password,
-        role_id = 1,
-    )
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
+        existing_user = db.query(User).filter(User.email == email).first()
+        if(existing_user):
+            return jsonify({
+                'status':False,
+                'message':'Email already used by someone'
+            })
+        
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256', salt_length=16)
 
-    apiToken = generate_jwt(new_user.id ,username)
-    new_user.set_api_token(apiToken)
-    db.commit()
-    
-    return jsonify({
-        'status':True,
-        'message':'Successfully account registered!',
-        'data':new_user.id
-    })
+        new_user = User(
+            name=username,
+            email=email,
+            password=hashed_password,
+            role_id = 1,
+        )
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+
+        apiToken = generate_jwt(new_user.id ,username)
+        new_user.set_api_token(apiToken)
+        db.commit()
+        
+        return jsonify({
+            'status':True,
+            'message':'Successfully account registered!',
+            'data':new_user.id
+        })
+    except Exception as e:
+        print(f"Error: {e}")
+        return jsonify({
+            'status':False,
+            'message':'Whoops!'
+        }) 
--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -11,7 +11,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(128), nullable=False)
     role_id = db.Column(db.String(4), nullable=True)
-    api_token = db.Column(db.String(256), nullable=True)
+    api_token = db.Column(db.String(255), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
     def set_password(self, password):
