9d362359b5d1172734e1103615b9d52104d0da33
Ajanthan <ajanthan.dev@hotelshippo.lk>
2025-09-11 11:11:43 +0530

    admin layout update

--- frontend/src/app/components/admin/dashboard/page.js
+++ frontend/src/app/components/admin/dashboard/page.js
@@ -1,12 +1,15 @@
 'use client'
 import AdminHeader from "../header/page"
+import AdminLayout from "../common/page"
 
 export default function AdminDashboard() {
     return (
         <>
-            <div>
-                <AdminHeader />
-            </div>
+            <AdminLayout>
+                <div>
+                    Welcome to Admin Dashboard
+                </div>
+            </AdminLayout>
         </>
     )
 }
