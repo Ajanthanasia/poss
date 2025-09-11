1481bb4aa1df345216871910ed6d56d34031484e
Dhushaa <dushadushanthani479@gmail.com>
2025-09-11 11:36:08 +0530

    Added admin dashboard sidebar and updated dashboard page

--- frontend/src/app/components/admin/dashboard/page.js
+++ frontend/src/app/components/admin/dashboard/page.js
@@ -1,11 +1,13 @@
 'use client'
 import AdminHeader from "../header/page"
+import AdminSidebar from "../sidebar/page"
 
 export default function AdminDashboard() {
     return (
         <>
             <div>
                 <AdminHeader />
+                  <AdminSidebar />
             </div>
         </>
     )
