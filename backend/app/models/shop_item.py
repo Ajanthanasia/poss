9d577f60ba26357bc3aaec86ef129bdeea3b27df
Vishalini22 <vinobavishalini@gmail.com>
2026-03-14 20:40:50 +0530

    products add

--- backend/app/models/shop_item.py
+++ backend/app/models/shop_item.py
@@ -2,7 +2,7 @@
 from app import db
 
 class ShopItem(db.Model):
-    __tablename__ = 'shop_items'
+    __tablename__ = 'products'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     shop_id = db.Column(db.Integer, nullable=False)
